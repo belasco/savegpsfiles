c555f3a20f507726ce5b60fb008457c4f2067832
Daniel Belasco Rogers <dan@planbperformance.net>
2017-12-24 12:06:49 +0100

    Change email address

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@
 setup.py
 2014/04/10 20:17:29
 
-Copyright 2014 Daniel Belasco Rogers danbelasco@yahoo.co.uk
+Copyright 2014 Daniel Belasco Rogers dan@planbperformance.net
 """
 
 try:
@@ -25,7 +25,7 @@ setup(name='savecurrentgps',
       description='savecurrentgps',
       long_description=readme(),
       author='Daniel Belasco Rogers',
-      author_email='danbelasco@yahoo.co.uk',
+      author_email='dan@planbperformance.net',
       url='https://github.com/belasco/savegpsfiles.git',
       download_url='https://github.com/belasco/savegpsfiles.git',
       version=__version__,
