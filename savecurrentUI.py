10657828ed3f0038e228b5d74f5e80497ba5d8c6
Daniel Belasco Rogers <dan@planbperformance.net>
2017-12-25 13:44:52 +0100

    Defining windows

--- savecurrentUI.py
+++ savecurrentUI.py
@@ -12,12 +12,86 @@ Copyright 2017 Daniel Belasco Rogers dan@planbperformance.net
 
 import sys
 import curses
-from savecurrentgps import *
+from savecurrentgps import parse_arguments, getsettings
 
 
-def main():
+def askyesno(question):
+    """
+    Ask a yes or no question. Loop until the answer is returned.
+    Answering 'q' quits the programme
+    """
+    while 1:
+        answer = input(question).lower()
+        if answer in ('y', 'yes'):
+            answer = True
+        elif answer in ('n', 'no'):
+            answer = False
+        elif answer == 'q':
+            print("You pressed q for Quit... Goodbye")
+            print()
+            sys.exit()
+        return answer
+
+        print('Please answer y or n')
+    return
+
+
+def chooseuser(userdict, maxyx):
+    """
+    using the user dictionary from the settings file, ask who's GPS
+    this is
+    """
+    margin = 4  # padding all round window
+    msg = "Please choose who this GPS belongs to from the list below:"
+    userwin = curses.newwin(maxyx[0] - margin * 2,
+                            maxyx[1] - margin * 2, margin, margin)
+    userwin.box()
+    userwin.addstr(2, 4, msg)
+
+    for idx, key in enumerate(sorted(userdict.keys())):
+        msg = "{}:  {}".format(key, userdict[key])
+        userwin.addstr(6 + idx, 10, msg)
+
+    userwin.refresh()
+
+    while 1:
+        msg = "Enter a number from 1 to {} > ".format(len(userdict))
+        user_uid = input("Enter a number from 1 to {} > ".format(len(userdict)))
+        user_uid = int(user_uid)
+        if user_uid in userdict.keys():
+            # confirm the selection
+            print()
+            ans = askyesno('You selected {}. Is this correct? '.format(userdict[user_uid]))
+            if ans:
+                return user_uid
+            else:
+                print('Please run the script again to select the correct user')
+                print('Script ends here\n')
+                sys.exit()
+
+        elif user_uid.lower() == 'q':
+            print("You pressed q for Quit... Goodbye")
+            print
+            sys.exit()
+
+        print('Your entry was not valid. Please try again.')
+
+
+def initscreen(myscreen):
+    curses.start_color()
+    curses.init_pair(1, curses.COLOR_RED, curses.COLOR_BLUE)
+
+    myscreen.bkgd(curses.color_pair(1))
+    myscreen.border(0)
+    myscreen.refresh()
+
+
+def main(myscreen):
     """
     """
+    initscreen(myscreen)
+    maxyx = myscreen.getmaxyx()
+
     args = parse_arguments()
 
     # read settings
@@ -25,6 +99,14 @@ def main():
         originaldirname, preprocessdirname, tempfilelocation,\
         userdict = getsettings(args.settingspath)
 
+    # get user from dict derived from settings
+    user_uid = chooseuser(userdict, maxyx)
+    name = userdict[user_uid]
+
 
 if __name__ == '__main__':
-    sys.exit(main())
+    try:
+        curses.wrapper(main)
+    except KeyboardInterrupt:
+        print("Got KeyboardInterrupt exception. Exiting...")
+        exit()
