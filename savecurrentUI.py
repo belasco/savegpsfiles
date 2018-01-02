320db732f675727265a6ff34585f5a07161efe85
Daniel Belasco Rogers <dan@planbperformance.net>
2018-01-02 18:12:38 +0100

    Getting closer to goal
    Wrote menu and info objects for various screens, looping over user
    confirmation, garmin mount check etc. TODO: make info object split
    title into a list so that strings of longer length than window can
    be formatted and used.

--- savecurrentUI.py
+++ savecurrentUI.py
@@ -10,90 +10,119 @@ An attempt to build an ncurses UI for savecurrentgps
 Copyright 2017 Daniel Belasco Rogers dan@planbperformance.net
 """
 
-import sys
 import curses
-from savecurrentgps import parse_arguments, getsettings
+from savecurrentgps import parse_arguments, getsettings,\
+     checkgarminmount, makenewfilename
 
 
-def askyesno(question):
+class Info(object):
+    """Put a string in a box for the user"""
+
+    def __init__(self, maxyx, title):
+        self.margin = 4
+        self.window = curses.newwin(maxyx[0] - self.margin * 2,
+                                    maxyx[1] - self.margin * 2,
+                                    self.margin, self.margin)
+        self.window.box()
+        self.window.keypad(1)
+        self.title = title
+
+    def display(self):
+        line = 2
+        margin = 4
+        self.window.addstr(line, margin, self.title)
+        self.window.addstr(line + 2, margin, 'Press any key.')
+
+        self.window.getch()
+
+        self.window.clear()
+        curses.doupdate()
+
+
+class Menu(object):
+    """Populate choices (items as a list) for the user to cycle through.
+
+    After instantiating the object, display it with the object's display
+    function. The answer is the answer property of the Menu object ie.
+    Menuname.answer
     """
-    Ask a yes or no question. Loop until the answer is returned.
-    Answering 'q' quits the programme
-    """
+    def __init__(self, items, maxyx, title):
+        self.margin = 4
+        self.window = curses.newwin(maxyx[0] - self.margin * 2,
+                                    maxyx[1] - self.margin * 2,
+                                    self.margin, self.margin)
+        self.window.box()
+        self.window.keypad(1)
+        self.position = 0
+        self.title = title
+        self.items = items
+
+    def display(self):
+        line = 2  # running total of what line we're on for layout
+        margin = 4
+        # self.msg = "Please choose who this GPS belongs to from the list below:"
+        self.window.addstr(line, margin, self.title)
+
+        line += 2
+        tab = margin + 4
+
+        while True:
+            self.window.refresh()
+            curses.doupdate()
+            for index, item in enumerate(self.items):
+                if index == self.position:
+                    mode = curses.A_REVERSE
+                else:
+                    mode = curses.A_NORMAL
+
+                self.window.addstr(line + index, tab, item, mode)
+
+            # get user input
+            key = self.window.getch()
+
+            if key in [curses.KEY_ENTER, ord('\n')]:
+                # the user has chosen the current position. Because we actually
+                # want the user_uid index for the userdict dictionary, plus 1 to
+                # position
+                self.answer = self.items[self.position]
+                break
+
+            elif key in [curses.KEY_UP, curses.KEY_LEFT, ord('p')]:
+                # navigate(-1)
+                self.position = self.position - 1
+
+            elif key in [curses.KEY_DOWN, curses.KEY_RIGHT, ord('n')]:
+                # navigate(1)
+                self.position = self.position + 1
+
+            # Modulo the dictionary length as a way of wrapping the menu round i.e.
+            # if under 0, go to max, if over max, go to 0 etc.
+            self.position = self.position % len(self.items)
+
+        self.window.clear()
+        curses.doupdate()
+
+
+def getuser(userdict, maxyx):
+    "Loop through asking for the user until confirmed"
+
     while 1:
-        answer = input(question).lower()
-        if answer in ('y', 'yes'):
-            answer = True
-        elif answer in ('n', 'no'):
-            answer = False
-        elif answer == 'q':
-            print("You pressed q for Quit... Goodbye")
-            print()
-            sys.exit()
-        return answer
+        # first question to find out user
+        title = "Please choose who this GPS belongs to from the list below:"
+        userlist = list(userdict.values())
+        usermenu = Menu(userlist, maxyx, title)
+        usermenu.display()
+        chosenuser = usermenu.answer
 
-        print('Please answer y or n')
-    return
+        # confirm
+        title = "You chose {}, is this correct?".format(chosenuser)
+        usermenu = Menu(['Yes', 'No'], maxyx, title)
+        usermenu.display()
 
-
-def chooseuser(userdict, maxyx):
-    """
-    using the user dictionary from the settings file, ask who's GPS
-    this is
-    """
-    margin = 4  # padding all round window
-    userwin = curses.newwin(maxyx[0] - margin * 2,
-                            maxyx[1] - margin * 2, margin, margin)
-    userwin.box()
-
-    line = 2  # running total of what line we're on
-    msg = "Please choose who this GPS belongs to from the list below:"
-    userwin.addstr(line, margin, msg)
-
-    line += 2
-    tab = margin + 4
-    for idx, key in enumerate(sorted(userdict.keys())):
-        msg = "{}:  {}".format(key, userdict[key])
-        userwin.addstr(line, tab, msg)
-        line += 1
-
-    msg = "Enter a number from 1 to {} > ".format(len(userdict))
-    line += 1
-    userwin.addstr(line, margin, msg)
-
-    userwin.refresh()
-
-    while 1:
-        user_uid = userwin.getch()
-        line += 1
-        if user_uid == ord('q'):
-            curses.endwin()
-            print("User entered Q for Quit. Script ends here.")
-            sys.exit()
-        elif user_uid not in userdict.keys():
-            msg = "That number is not in use. Please enter another. "
-            userwin.addstr(line, margin, msg)
-            
-
-        # user_uid = input("Enter a number from 1 to {} > ".format(len(userdict)))
-        # user_uid = int(user_uid)
-        # if user_uid in userdict.keys():
-        #     # confirm the selection
-        #     print()
-        #     ans = askyesno('You selected {}. Is this correct? '.format(userdict[user_uid]))
-        #     if ans:
-        #         return user_uid
-        #     else:
-        #         print('Please run the script again to select the correct user')
-        #         print('Script ends here\n')
-        #         sys.exit()
-
-        # elif user_uid.lower() == 'q':
-        #     print("You pressed q for Quit... Goodbye")
-        #     print
-        #     sys.exit()
-
-        # print('Your entry was not valid. Please try again.')
+        # only break out of this while loop when Yes is returned, otherwise ask
+        # first question again
+        if usermenu.answer == 'Yes':
+            return chosenuser
 
 
 def initscreen(myscreen):
@@ -102,6 +131,7 @@ def initscreen(myscreen):
 
     myscreen.bkgd(curses.color_pair(1))
     myscreen.border(0)
+    curses.curs_set(0)
     myscreen.refresh()
 
 
@@ -118,9 +148,29 @@ def main(myscreen):
         originaldirname, preprocessdirname, tempfilelocation,\
         userdict = getsettings(args.settingspath)
 
-    # get user from dict derived from settings
-    user_uid = chooseuser(userdict, maxyx)
-    name = userdict[user_uid]
+    # check if the garmin is mounted to the location specified in settings
+    garminfilelocation = checkgarminmount(garminfilelocation)
+
+    if not garminfilelocation:
+        info = Info(maxyx, 'No Garmin found. Please try again')
+        info.display()
+        exit(2)
+    else:
+        info = Info(maxyx, 'Garmin found, ready for import')
+        info.display()
+
+    # get and confirm user
+    name = getuser(userdict, maxyx)
+
+    # create the new file path using the various settings and
+    # calculated values
+    newfilepath = makenewfilename(basefilepath,
+                                  originaldirname,
+                                  name, curyear)
+
+    msg = "Saving GPX file as a compressed file in {}".format(newfilepath)
+    info = Info(maxyx, msg)
+    info.display()
 
 
 if __name__ == '__main__':
