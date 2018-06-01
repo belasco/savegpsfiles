29f010abf104b1220f478e9ca731e30914d311cb
Daniel Belasco Rogers <dan@planbperformance.net>
2018-06-01 15:40:13 +0200

    Lose test script and bump version

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__version__ = '0.6'
+__version__ = '0.7'
 CONFIGPATH = '~/.config/savecurrentgps/settings.cfg'
 FILESTRUCTHELPER = 'GPSfilestruct.sh'
