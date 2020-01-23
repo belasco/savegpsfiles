827090c0f643b6f3a443d3285b5fa5ef7fd08b93
Daniel Belasco Rogers <dan@planbperformance.net>
2020-01-23 22:01:56 +0100

    Bump to 0.8

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__version__ = '0.7'
+__version__ = '0.8'
 CONFIGPATH = '~/.config/savecurrentgps/settings.cfg'
 FILESTRUCTHELPER = 'archive_makefolders.py'
 DEFAULTBASE = '~/selectedplanbfiles/currentGPS'
